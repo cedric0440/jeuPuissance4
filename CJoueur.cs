1e6477b5d2f4c254bab1e04a51948469d58eed2f
Edgar Gegout <2435196@cegeprdl.ca>
2025-09-02 15:36:14 -0400

    Creation classe + nom methode et attribut

---
+++ CJoueur.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jeuPuissance4
+{
+
+    public class CJoueur
+
+    {
+        private string nom;
+        private bool gagnant;
+        private int checkPoints;
+
+        public Jeton GetJeton()
+        {
+
+        }
+
+        public void setJeton(string symbole, int points)
+        {
+            
+
+        }
+
+        public void ProcederChoix(ref Plateau leplateau, int colonne, int rangee)
+        {
+
+        }
+
+
+
+    }
+}
