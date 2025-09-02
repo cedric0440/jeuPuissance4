f10f1e97599be2e025605a3ec2809a8129cbefb9
Edgar Gegout <2435196@cegeprdl.ca>
2025-09-02 16:40:35 -0400

    Ajout des nom de methodes dans le controleur

--- ControleurPuissance4.cs
+++ ControleurPuissance4.cs
@@ -7,8 +7,45 @@ using System.Threading.Tasks;
 
 namespace jeuPuissance4
 {
-    public class ControleurPuissance4
+    public class ControleurPuissance4(string nomJoueur1, string nomJoueur2)
     {
+        private int compteurTour;
 
+        public void JouerTour(int joueur, int indiceColonne)
+        {
+
+        }
+        public string ObtenirPlateau()
+        {
+            return "aaa";
+        }
+        public string ObtenirMessageGagnant()
+        {
+            return "aaa";
+        }
+        public bool IsGagnant()
+        {
+            return true;
+        }
+        public int GetCompteurTour()
+        {
+            return 1;
+        }
+        public string GetSymbole(int numeroJoueur)
+        {
+            return "aaa";
+        }
+        public bool IsCaseDisponible(int colonne)
+        {
+            return true;
+        }
+        public void DeterminerGagnant(int joueur)
+        {
+
+        }
+        public bool IsTermine()
+        {
+            return true;
+        }
     }
 }
